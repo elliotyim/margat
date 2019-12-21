6e3e4c1204e5eac17494393c24cc7bafe4c93f93
yimsb01 <yimsb02@naver.com>
2019-12-21 22:42:02 +0900

    서버 IP주소 표시하는 js파일 커밋

---
+++ app/server/server_dir/server_ip.js
@@ -0,0 +1,17 @@
+var os = require('os');
+  
+module.exports = function getServerIp() {
+    var ifaces = os.networkInterfaces();
+    var result = '';
+    for (var dev in ifaces) {
+        var alias = 0;
+        ifaces[dev].forEach(function(details) {
+            if (details.family == 'IPv4' && details.internal === false) {
+                result = details.address;
+                ++alias;
+            }
+        });
+    }
+  
+    return result;
+}
