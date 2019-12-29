d8613678ed6da912ede60f7a75677b7c6fe65f01
yimsb01 <yimsb02@naver.com>
2019-12-29 15:18:55 +0900

    코드 정리

--- app/server/service/post_service.js
+++ app/server/service/post_service.js
@@ -4,38 +4,35 @@ const photoService = require('./photo_service')
 module.exports = {
   getBasicSqlColumns() {
     let sqlColumns = 'post_no as postNo,'
-                +'mem_no as memberNo,'
-                +'post_cont as postContent,'
-                +'post_cdt as postCreatedDate'
-    let inner = () => {return sqlColumns}
+      + 'mem_no as memberNo,'
+      + 'post_cont as postContent,'
+      + 'post_cdt as postCreatedDate'
+    let inner = () => { return sqlColumns }
     return inner;
   },
   getAllPostsByUser(req, res) {
     let no = req.params.no;
-    
+
     mysqlDB.query(
       'select ' + this.getBasicSqlColumns()() +
       ' from posts where mem_no=? order by post_cdt desc',
-    [no],
-    (err, rows) => {
-      if (err) res.send(err)
-      else if (rows) {
-        ((rows) => {
-          return new Promise((resolve) => {
-            for (let i = 0; i < rows.length; i++) {
-              photoService.getAllPhotosBy(rows[i].postNo)
-                .then((result) => {
-                  rows[i].photos = result
-                  console.log(rows);
-                  console.log(rows[0].photos);
-                  console.log(JSON.stringify(rows[0].photos));
-                  resolve(rows);
-                  if (i == rows.length-1) res.send(rows);
-                });
-            }
-          })
-        })(rows);
-      }
-    });
+      [no],
+      (err, rows) => {
+        if (err) res.send(err)
+        else if (rows) {
+          ((rows) => {
+            return new Promise((resolve) => {
+              for (let i = 0; i < rows.length; i++) {
+                photoService.getAllPhotosBy(rows[i].postNo)
+                  .then((result) => {
+                    rows[i].photos = result
+                    resolve(rows);
+                    if (i == rows.length - 1) res.send(rows);
+                  });
+              }
+            })
+          })(rows);
+        }
+      });
   }
 }
