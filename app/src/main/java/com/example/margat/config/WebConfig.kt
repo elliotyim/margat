d3faa749803c19c2b9630a31d8683b8e072de552
yimsb01 <yimsb02@naver.com>
2019-12-31 00:28:14 +0900

    서버의 경로 변경

--- app/src/main/java/com/example/margat/config/WebConfig.kt
+++ app/src/main/java/com/example/margat/config/WebConfig.kt
@@ -1,11 +1,8 @@
 package com.example.margat.config
 
 open class WebConfig {
-//    protected val ipAddress: String = "http://192.168.35.90"
-//    protected val portNo: String = "8080"
-
     companion object {
-        val ipAddress: String = "http://192.168.35.90"
-        val portNo: String = "8080"
+        const val ipAddress: String = "http://192.168.0.125"
+        const val portNo: String = "8080"
     }
 }
